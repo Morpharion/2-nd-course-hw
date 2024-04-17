adfe538fd0b13d0d1410ba47771d6018fa9eda46
Andrew Petrov <andre.petroff2010@yandex.ru>
2024-04-17 23:16:37 +0300

    hw 2.5/2.6

--- hw-2.6.js
+++ hw-2.6.js
@@ -3,27 +3,25 @@
 const numbs = [1, 5, 4, 10, 0, 3];
 
 for (let i = 0; i < numbs.length; i++) {
+    console.log(numbs[i]);
 
-    if (numbs[i] == 10) {
+    if (numbs[i] === 10) {
         console.log(numbs[i]);
         break;
-    }
-    else {
-        console.log(numbs[i]);
     }
 }
 
 // Задание 2
 
-const numbers = [1, 5, 4, 10, 0, 3];
+const numbersArray = [1, 5, 4, 10, 0, 3];
 
-const index = numbers.indexOf(4);
+const index = numbersArray.indexOf(4);
 console.log(index);
 
 // Задание 3
 
-const numbers = [1, 3, 5, 10, 20];
-const result = numbers.join(' ');
+const numbersArraySecond = [1, 3, 5, 10, 20];
+const result = numbersArraySecond.join(' ');
 console.log(result);
 
 // Задание 4
@@ -31,12 +29,14 @@ console.log(result);
 const n = 3;
 const m = 3;
 
-const array = new Array(n).fill(0).map(() => new Array(m).fill(0));
+const array = [];
 
 for (let i = 0; i < n; i++) {
+    const innerArray = [];
     for (let j = 0; j < m; j++) {
-        array[i][j] = 1;
+        innerArray.push(1);
     }
+    array.push(innerArray);
 }
 
 console.log(array);
@@ -52,8 +52,10 @@ console.log(numbers);
 
 const massive = [9, 8, 7, 'a', 6, 5];
 
-massive.sort();
-const newArr = massive.filter((element) => element !== 'a');
+const newArr = massive.filter((element) => {
+    return typeof element === 'number';
+});
+
 console.log(newArr);
 
 // Задание 7
@@ -62,12 +64,12 @@ const numbers = [9, 8, 7, 6, 5];
 
 const guess = prompt("Угадай число!");
 const guessNumber = Number(guess);
-const guessed = includes(guessNumber);
+const guessed = numbers.includes(guessNumber);
 
 if (guessed) {
-    console.log("Угадал!");
+    alert("Угадал!");
 } else {
-    console.log("Не угадал");
+    alert("Не угадал");
 }
 
 // Задание 8
@@ -79,19 +81,21 @@ console.log(reversedletters);
 // Задание 9
 
 const massive = [[1, 2, 3], [4, 5, 6]];
-const NewMssive = [...massive[0], ...massive[1]];
+const newMassive = [...massive[0], ...massive[1]];
 console.log(NewMssive);
+
+// Задание 9. Через flat()
+
+const massive = [[1, 2, 3], [4, 5, 6]];
+const newMassive = massive.flat(1);
+console.log(newMassive);
 
 // Задание 10
 
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 for (let i = 0; i < numbers.length - 1; i++) {
-    const currentNumber = numbers[i];
-    const nextNumber = numbers[i + 1];
-    const sum = currentNumber + nextNumber;
-
-    console.log(`Сумма ${currentNumber} и ${nextNumber} равна ${sum}`);
+    console.log(`Сумма ${numbers[i]} и ${numbers[i + 1]} равна ${numbers[i] + numbers[i + 1]}`);
 }
 
 // Задание 11
@@ -134,14 +138,13 @@ console.log(result);
 
 const numbers = [];
 for (let i = 0; i < 10; i++) {
-    numbers.push(Math.floor(Math.random() * 10) + 1);
+    numbers.push(Math.floor(Math.random() * 11));
 }
 
 const evenNumbers = numbers.filter((number) => number % 2 === 0);
 
 console.log("Исходный массив:", numbers);
 console.log("Массив четных значений:", evenNumbers);
-Math.floor()
 
 // Задание 15
 
@@ -156,5 +159,3 @@ const average = sum / numbers.length;
 
 console.log("Исходный массив:", numbers);
 console.log("Среднее арифметическое:", average);
-
-sumasdasd
